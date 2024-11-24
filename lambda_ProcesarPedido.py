aa75989a3d198364a72b4f4b90446ea960b8ae2f
stewartmb <165910969+stewartmb@users.noreply.github.com>
2024-11-24 16:07:04 -0500

    Update lambda_ProcesarPedido.py

--- lambda_ProcesarPedido.py
+++ lambda_ProcesarPedido.py
@@ -2,12 +2,14 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-
-    # Create SQS client
+    # Crear cliente SQS
     sqs = boto3.client('sqs')
-    queue_url = 'https://sqs.us-east-1.amazonaws.com/498917627164/sqs-pedidos'
-    
-    # Receive message from SQS queue
+    queue_url = 'https://sqs.us-east-1.amazonaws.com/630606600000/sqs-pedidos'
+
+    # Obtener despachador_id del evento
+    despachador_id = event["despachador_id"]  # se obtiene el despachador_id
+
+    # Recibir mensajes de la cola SQS
     response = sqs.receive_message(
         QueueUrl=queue_url,
         MaxNumberOfMessages=3,
@@ -15,7 +17,6 @@ def lambda_handler(event, context):
     )
     
     print(response)
-    
     messages = response.get('Messages', [])
     pedidos = []
     dynamodb = boto3.resource('dynamodb')
@@ -23,18 +24,25 @@ def lambda_handler(event, context):
 
     for message in messages:
         pedido = json.loads(message['Body'])
-        print(pedido) 
-        pedidos.append(pedido)
+        print(pedido)
+
+        # Agregar despachador_id al pedido
+        pedido['despachador_id'] = despachador_id
+
+        # Almacenar el pedido en DynamoDB
         response_dynamodb = table.put_item(Item=pedido)
+        pedidos.append(pedido)
+
+        # Eliminar mensaje de la cola SQS
         receipt_handle = message['ReceiptHandle']
-        # Delete received message from queue
         sqs.delete_message(
             QueueUrl=queue_url,
             ReceiptHandle=receipt_handle
         )
-
+    
+    # Respuesta final
     return {
         'statusCode': 200,
-        'pedidos_procesados': pedidos
+        'cantidad_pedidos_procesados': len(pedidos),  # Número de pedidos procesados
+        'pedidos_procesados': pedidos  # Lista de pedidos procesados
     }
-    
